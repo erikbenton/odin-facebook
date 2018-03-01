ba9203050961ab1deea9d27c2a90501787eec2cc
erikbenton <erik.benton@hotmail.com>
2018-03-01 15:42:19 -0500

    adding named tabs to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  
+  root 					 	 'static_pages#home'
+  get		'about' => 'static_pages#about'
 
-  get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
