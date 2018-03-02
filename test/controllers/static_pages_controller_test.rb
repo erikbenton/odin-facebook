60671cd79b8e85f6b0c0cc9c773b6a5dfbc6af81
erikbenton <erik.benton@hotmail.com>
2018-03-02 12:04:28 -0500

    adding bootstrap and styling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,16 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
+    assert_select "title", "Odin Facebook"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
+    assert_select "title", "About | Odin Facebook"
   end
 
 end
