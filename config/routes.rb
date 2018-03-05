deab03724621278370a17b67989309bdfce11408
erikbenton <erik.benton@hotmail.com>
2018-03-05 14:34:24 -0500

    making adjustments to user model and application controller so user's name can be seen

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 					 		'static_pages#home'
   get		'/login' => 'static_pages#home'
   get		'/about' => 'static_pages#about'
+  
   devise_for :users
+  # devise_for :users, controllers: { registrations: "users"}
+  
   resources :users, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302203342) do
+ActiveRecord::Schema.define(version: 20180305192409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -29,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180302203342) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
