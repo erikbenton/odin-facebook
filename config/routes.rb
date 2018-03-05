445f57f99b0b78c117344d7ffee428b45232a393
erikbenton <erik.benton@hotmail.com>
2018-03-05 13:38:40 -0500

    adding simple routing for viewing user

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
-  
-  get 'users/index'
 
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  devise_for :users
   root 					 		'static_pages#home'
   get		'/login' => 'static_pages#home'
   get		'/about' => 'static_pages#about'
-
+  devise_for :users
+  resources :users, only: [:index, :show]
 end
