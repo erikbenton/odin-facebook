55c683fa7e272768e6c1bc6d52ca6852d9032945
erikbenton <erik.benton@hotmail.com>
2018-03-05 12:08:04 -0500

    adding tests for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@erik = User.new(name: "Erik", email: "erik@example.com", password: "foo123")
+  end
+
+  test 'should be valid' do
+  	assert @erik.valid?
+  end
+
+  test 'should downcase email' do
+  	mixed_case_email = "WeLcOME@emaIL.CoM"
+  	@erik.email = mixed_case_email
+  	@erik.save
+  	assert_equal mixed_case_email.downcase, @erik.reload.email
+  end
+
 end
