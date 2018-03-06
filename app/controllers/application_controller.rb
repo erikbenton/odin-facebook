fa00e3da5719b8fd9dcfe3e073e9c22dd145c18e
erikbenton <erik.benton@hotmail.com>
2018-03-06 15:59:53 -0500

    adding ability to edit profile/user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 	  def configure_permitted_parameters
 	  	attributes = [:name, :email, :password, :password_confirmation, :remember_me]
     	devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
-    	devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    	devise_parameter_sanitizer.permit(:account_update, keys: attributes)
 	  end
 
 end
