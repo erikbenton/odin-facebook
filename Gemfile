2c80844d0c0d1a6e4809197cbb119db53d54ccea
erikbenton <erik.benton@hotmail.com>
2018-03-09 08:53:51 -0500

    adding faker for seeding db

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'puma', '~> 3.7'
 gem 'devise'
 # Using Omniauth
 gem 'omniauth'
+# Using Faker for populating site
+gem 'faker'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Erik Benton",
+						 email: 'erik@example.com',
+						 password: 'password',
+						 password_confirmation: 'password')
+
+99.times do |n|
+	name = Faker::TwinPeaks.character
+	email = "example-#{n+1}@example.com"
+	password = 'password'
+	password_confirmation = 'password'
+	User.create!(name: name, email: email, password: password, password_confirmation: password_confirmation)
+end
