ab4ec6fafbf477c6860e4047f7a4254e75b3ada5
erikbenton <erik.benton@hotmail.com>
2018-03-09 15:38:19 -0500

    adding controller for friendships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def notifications
+  	@user = User.find(params[:id])
+  	@friend_invites = @user.received_friendships
+  	render 'show_notifications'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users, controllers: { registrations: "registrations" }
   
+  # Setting up route for getting notifications
+  resources :users do
+  	member do
+  		get :notifications
+  	end
+  end
   resources :users, only: [:index, :show]
 end
