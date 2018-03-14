77e668a0cf4f7133e5978e7acb91c44566729527
erikbenton <erik.benton@hotmail.com>
2018-03-14 11:04:56 -0400

    fixing model for likes

--- db/migrate/20180314145901_create_likes.rb
+++ db/migrate/20180314145901_create_likes.rb
@@ -6,7 +6,6 @@ class CreateLikes < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-    add_index :likes, [:user_id]
-    add_index :likes, [:post_id]
+    add_index :likes, [:user_id, :post_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312181421) do
+ActiveRecord::Schema.define(version: 20180314145901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20180312181421) do
     t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
     t.index ["sender_id", "receiver_id"], name: "index_friendships_on_sender_id_and_receiver_id", unique: true
     t.index ["sender_id"], name: "index_friendships_on_sender_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -53,5 +63,7 @@ ActiveRecord::Schema.define(version: 20180312181421) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
